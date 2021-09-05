0c79f99aba1393e27e3ae711b64de1bdb5089a0b
Anonymous <>
2021-09-05 18:04:38 +0300

    Add Search Filter Functionality

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,39 @@
-import { Table } from "antd";
+import { useState } from "react";
+import { Table, Space, Input, Button, Row, Col } from "antd";
+import { FilterOutlined, DownloadOutlined } from "@ant-design/icons";
 import { data, columns } from "../data";
 import "antd/dist/antd.css";
 const TableComponent = () => {
-
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = ({ target }) => setSearchTerm(target.value);
+  console.log(data.filter((d) => d.service.includes("a")));
   return (
     <>
-      <Table columns={columns} dataSource={data} rowSelection={true} pagination={{pageSize: 8}}/>
+
+        <Row justify="space-between">
+          <Col >
+            <Input.Search
+              style={{ width: "320px" }}
+              placeholder="Fatura Ara"
+              onChange={handleChange}
+            />
+          </Col>
+          <Col>
+            <Button icon={<FilterOutlined />}></Button>
+            <Button icon={<DownloadOutlined />} style={{marginLeft: "1em"}}/>
+          </Col>
+        </Row>
+      <Table
+        columns={columns}
+        dataSource={
+          searchTerm === ""
+            ? data
+            : data.filter((d) => d.service.toLowerCase().includes(searchTerm))
+        }
+        rowSelection={true}
+        pagination={{ pageSize: 8 }}
+        filters={true}
+      />
     </>
   );
 };
